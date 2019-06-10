e22972bd4bd70b6f97644b5b0c4ae1d9b1600a0b
NosaPrecious <Okundaye.preciousnosa@gmail.com>
2019-06-10 12:09:21 -0400

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,33 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+//   i ← 1
+// while i < length(A)
+//     j ← i
+//     while j > 0 and A[j-1] > A[j]
+//         swap A[j] and A[j-1]
+//         j ← j - 1
+//     end while
+//     i ← i + 1
+// end while
+// debugger
+
+let i = 1, j
+while(i < arr.length-1){
+   j = 1
+  while(j > 0 && arr[j-1] > arr[j]){
+    swapVal(arr[j], arr[j-1])
+    j = j - 1
+  }
+  i = i + 1
+}
+
+}
+
+function swapVal(a,b){
+  let temp
+  temp = a
+  a = b
+  b = temp
+  return [a,b]
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,32 @@
 function selectionSort(arr) {
+  // debugger
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  let i, j, minVal
+  let arrLen= arr.length
+
+  for(i = 0; i < arrLen-1; i++){
+    minVal = i
+  }
+
+  for(j=i+1; j < arrLen; j++){
+    if(arr[j] < arr[minVal]){
+      minVal = j
+    }
+  }
+
+  if(minVal != i){
+    swapVal(arr[i], arr[minVal])
+  }
+  // debugger
+
+}
+
+
+function swapVal(a,b){
+  let temp
+  temp = a
+  a = b
+  b = temp
+  return [a,b]
 }
